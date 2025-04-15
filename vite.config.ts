d25dab1d02d56b852e7b76370a75ea50009da691
ssp <qkrtjdtlr02@naver.com>
2025-04-15 14:39:13 +0900

    배포 관련 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        basic: resolve(__dirname, 'index.basic.html'),
+        advanced: resolve(__dirname, 'index.advanced.html')
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
